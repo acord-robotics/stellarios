34003cbc14c7558ea67b962ac68ce4c44bf0a285
Liam <droidology@allianceofdroids.org.au>
2019-08-30 13:49:36 +0800

    Python-Learning

--- learnpython.org/Hello World!.py
+++
@@ -1,2 +0,0 @@
-print("Hello, World!")
-# This prints "Hello World" to the console
--- randomnamegenerator.py
+++
@@ -1,2 +0,0 @@
-import time
-print("Hello, and welcome back. I hope you enjoyed the previous mini-app I created for DroidOS Python")
