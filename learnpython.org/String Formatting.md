1e5ebd21bbb75878e20e2564c70004d0b37c5e48
Liam <droidology@allianceofdroids.org.au>
2019-08-30 12:08:34 +0800

    String Formatting

--- learnpython.org/String Formatting.md
+++ learnpython.org/String Formatting.md
@@ -22,3 +22,15 @@ age = 16
 print("%s is %d years old" % (name, age))
 ```
 
+Any object which is not a string can be formatted in the same way:
+
+```python
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+```
+
+```
+%s - String (or any object with a string representation, like numbers)` `%d - Integers` `%f - Floating point numbers` `%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.` `%x/%X - Integers in hex representation (lowercase/uppercase)
+```
+
--- learnpython.org/String Formatting.py
+++ learnpython.org/String Formatting.py
@@ -6,3 +6,19 @@ print("Hello, %s!" % name) # %s refers to the name variable (take not of where t
 name = "Liam"
 age = 16
 print("%s is %d years old" % (name, age))
+
+# List
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+# Other formatters
+%s - #String (or any object with a string representation, like numbers)
+
+%d - #Integers
+
+%f - #Floating point numbers
+
+%.<number of digits>f - #Floating point numbers with a fixed amount of digits to the right of the dot.
+
+%x/%X - Integers in hex # representation (lowercase/uppercase)
