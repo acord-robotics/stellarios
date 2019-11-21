189e6f4ef4361622fbd5042e1cb00809ad4666df
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-11-21 17:26:00 +0800

    ACoRD: Stellarios: GH/ID/PL reorg

--- learnpython.org/String Formatting.py
+++
@@ -1,8 +0,0 @@
-# This prints out "Hello, John!"
-name = "John"
-print("Hello, %s!" % name) # %s refers to the name variable (take not of where the exclamation mark is placed)
-
-# %d, %s
-name = "Liam"
-age = 16
-print("%s is %d years old" % (name, age))
