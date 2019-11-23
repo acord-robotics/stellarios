dd39faa102152a7f3f002449019690994690fff7
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-11-23 23:04:10 +0800

    Update player.cs

--- Applications/RPG Brackeys/Assets/Scripts/player.cs
+++ Applications/RPG Brackeys/Assets/Scripts/player.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Player : MonoBehaviour {
+
+    /// <summary>
+    /// The Player's movement speed
+    /// </summary>
+    [SerializeField]
+    private float speed = 1; // when a variable is private, only the player/entity it is assigned to can access it
+
+    /// <summary>
+    /// The Player's direction
+    /// </summary>
+    private Vector2 direction;
+
+
+
+	// Update is called once per frame
+	void Update ()
     {
-        
+        //Executes the GetInput function
+        GetInput();
+
+        //Executes the Move function
+        Move();
+	}
+
+    /// <summary>
+    /// Moves the player
+    /// </summary>
+    public void Move()
+    {
+        //Makes sure that the player moves
+        transform.Translate(direction*speed*Time.deltaTime); // the entity will move at the same speed on all devices (not dependant on FPS)
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Listen's to the players input
+    /// </summary>
+    private void GetInput()
     {
-        
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A)) // If "A" key is pressed, move to the left (line 52) - this follows for everything in this "private void GetInput()" at line 42
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
     }
 }
